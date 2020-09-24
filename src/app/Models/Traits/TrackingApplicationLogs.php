666abdbc0df919e405e6c84ef718adab0aa1fda2
matteodeliperi <matteo.deliperi@smart-contact.it>
2020-09-24 10:26:45 +0200

    Delete helper

--- src/app/Models/Traits/TrackingApplicationLogs.php
+++ src/app/Models/Traits/TrackingApplicationLogs.php
@@ -76,7 +76,7 @@ trait TrackingApplicationLogs
             ];
         }
 
-        $getAgent = getAgent();
+        $getAgent = $this->getAgent();
 
         $registerNewLogin = array_merge($baseInfoUserArray, $getAgent);
         ApplicationLog::create($registerNewLogin);
@@ -122,5 +122,34 @@ trait TrackingApplicationLogs
         ];
 
         $this->registerApplicationLog(null, $baseInfoUserArray);
+    }
+
+    /**
+     * @return array
+     */
+    function getAgent()
+    {
+        $device = new \Jenssegers\Agent\Agent();
+        $browser = $device->browser();
+        $platform = $device->platform();
+
+        return [
+            "user_agent" => $device->getUserAgent(),
+            "browser" => $browser ?? null,
+            "browser_version" => $device->version($browser) ?? null,
+            "platform" => $platform ?? null,
+            "platform_version" => null,
+            "ip" => $this->getClientIpAddress(),
+        ];
+    }
+
+    /**
+     * @return mixed|string
+     */
+    function getClientIpAddress()
+    {
+        return request()->server->get('HTTP_X_FORWARDED_FOR') ?
+            explode(',', request()->server->get('HTTP_X_FORWARDED_FOR'))[0] :
+            request()->server->get('REMOTE_ADDR');
     }
 }

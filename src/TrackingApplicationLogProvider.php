29408fc696619ac4d139f148b6a138debdb6fff2
matteodeliperi <matteo.deliperi@smart-contact.it>
2020-09-28 15:59:40 +0200

    Fix policy

--- src/TrackingApplicationLogProvider.php
+++ src/TrackingApplicationLogProvider.php
@@ -27,6 +27,8 @@ class TrackingApplicationLogProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'tracking-application-log');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        $this->gate();
     }
 
     /**
